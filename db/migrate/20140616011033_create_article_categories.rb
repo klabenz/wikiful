2f43f57555407692a1f32101f9128497b033d87d
klabenz <klabenz@karenlabenz.com>
2014-06-16 18:23:58 -0400

    created articleCategories and played with Console

--- db/migrate/20140616011033_create_article_categories.rb
+++ db/migrate/20140616011033_create_article_categories.rb
@@ -1,4 +1,4 @@
-class CreateArticleCategories < ActiveRecord::Migration
+iesclass CreateArticleCategories < ActiveRecord::Migration
   def change
     create_table :article_categories do |t|
     	t.belongs_to :article
